280db64853e7e74418794cb72fe2cc6a6afd86bc
elifozer <elifozer173@gmail.com>
2020-08-31 13:45:37 +0300

    Added Book model Implemented Book model db migration

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // In order to add any model to the database, we need an entry here
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    // Models represent any table we want in the database
+
+    public class Book
+    {
+        // This will create an Id value automatically, we do not need to pass a value
+        [Key]
+        public int Id { get; set; }
+
+        // This means Name cannot be null
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
